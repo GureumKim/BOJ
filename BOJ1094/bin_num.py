67fc7ff2f83e95b6601b4a3001b6143e96a4ec89
GureumKim <greeneerg08@gamil.com>
2023-07-21 20:44:07 +0900

    bin_num.py 수정

--- BOJ1094/bin_num.py
+++ BOJ1094/bin_num.py
@@ -1,5 +1,5 @@
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readline #이렇게 안해도 되지만 나중을 위해 미리 연습겸 사용했다.
 
 # f-string 연습 겸, 문제 조건을 고려할 겸 7칸을 확보하여 공백을 0으로 표현했다.
 num = f'{int(input().rstrip()):>07b}'
