3a6f83ac9915bb3c2e08537fab71f3d01d954fdd
GureumKim <greeneerg08@gamil.com>
2023-07-26 20:31:49 +0900

    방법2 추가

--- BOJ1251/1251.py
+++ BOJ1251/1251.py
@@ -5,6 +5,7 @@
 : 아직 이중 포문에서 인덱스를 활용한 문제 풀이가 익숙치 못하다.
 특히 인덱스 범위 지정이 아직 많이 헷갈린다. 이런 문제를 많이 풀어야겠다. 
 '''
+# 방법 1, 리스트 append 이용
 s = input()
 l = len(s)-1
 
@@ -15,3 +16,15 @@ for i in range(l-1):
         results.append(result)
 results.sort()
 print(results[0])
+
+# 방법 2 한 번에 다 해버리기
+s = input()
+l = len(s)-1
+
+first = 'z'*50
+for i in range(l-1):
+    for j in range(i+1,l):
+        result = s[i::-1]+s[j:i:-1]+s[-1:j:-1]
+        if first > result:
+            first = result
+print(first)
