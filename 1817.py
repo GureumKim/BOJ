d0d4cb63bf81f2857e5727a151cbc989cac39304
GureumKim <greeneerg08@gmail.com>
2023-08-29 09:04:24 +0900

    1817.py

---
+++ 1817.py
@@ -0,0 +1,17 @@
+import sys;input=sys.stdin.readline
+n, m = map(int,input().split())
+if n:       #상자에 들어갈 게 없을 때 처리
+    weights = list(map(int,input().split()))
+    stack = []
+    cnt = 1
+    s = 0
+    for w in weights:
+        if s + w <= m:
+            s += w
+        else:
+            s = w
+            cnt += 1
+    print(cnt)
+else:
+    print(0)
+# try & except 굳이 필요 없음..
