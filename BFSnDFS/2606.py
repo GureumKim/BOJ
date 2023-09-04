f499f3aea7ce0c61d8559beb8c23d8b6418d4ef3
GureumKim <greeneerg08@gmail.com>
2023-09-05 01:05:59 +0900

    BnDFS

---
+++ BFSnDFS/2606.py
@@ -0,0 +1,23 @@
+import sys; input = sys.stdin.readline
+from collections import deque
+
+num = int(input())
+adj_lst = [[] for _ in range(num+1)]
+infected = [0]*(num+1)
+
+q = deque()
+ans = 0
+for _ in range(int(input())):
+    s, e = map(int,input().split())
+    adj_lst[s].append(e)
+    adj_lst[e].append(s)
+infected[1] = 1
+q.append(1)
+while q:
+    now = q.popleft()
+    for com in adj_lst[now]:
+        if infected[com]: continue 
+        infected[com] = 1
+        q.append(com)
+        ans +=1
+print(ans)
