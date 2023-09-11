702de4dda8806862e08e0c47be4fea64722648d1
GureumKim <greeneerg08@gmail.com>
2023-09-11 17:47:06 +0900

    update

--- n_m_series.py
+++ n_m_series.py
@@ -56,3 +56,42 @@ from itertools import combinations_with_replacement as c_w_r
 n, m = map(int,input().split())
 lst = [str(i) for i in range(1,n+1)]
 print("\n".join(map(' '.join,c_w_r(lst,m))))
+
+
+
+# 5
+def dfs(level=0):
+    if level == m:
+        print(*choice)
+        return
+    for i in range(n):
+        if used[i]: continue
+        used[i] = 1
+        choice[level] = lst[i]
+        dfs(level+1)
+        used[i] = 0
+
+n, m = map(int,input().split())
+lst = list(map(int,input().split()))
+lst.sort()
+choice = [0]*m
+used = [0]*n
+dfs()
+
+# 5 -2
+from itertools import permutations as pm
+n,m = map(int,input().split())
+lst = list(input().split())
+print('\n'.join(map(' '.join,pm(sorted(lst,key=lambda x:(len(x),x)),m))))
+
+#6
+from itertools import combinations as comb
+n,m = map(int,input().split())
+lst = list(input().split())
+print('\n'.join(map(' '.join,comb(sorted(lst,key=lambda x:(len(x),x)),m))))
+
+# 7
+from itertools import product
+n,m = map(int,input().split())
+lst = list(input().split())
+print('\n'.join(map(' '.join,product(sorted(lst,key=lambda x:(len(x),x)),repeat=m))))
