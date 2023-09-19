c4eecb222f10d0785ac7e81261ae50ec2fb5c52d
GureumKim <greeneerg08@gmail.com>
2023-09-19 21:41:04 +0900

    update

--- dajkstra/18352.py
+++ dajkstra/18352.py
@@ -12,26 +12,25 @@ def dajkstra(start):
 
         for i in adj[via]:
             cost = v_cost + 1
-            if cost < result[i-1]:
+            if cost < result[i]:
                 result[i] = cost
                 hq.heappush(heap,(cost,i))
 
 
 n, m, k, x = map(int,input().split())
-adj = [[] for _ in range(n)]
+adj = [[] for _ in range(n+1)]
 for _ in range(m):
     s, e = map(int,input().split())
     adj[s].append(e)
 
 inf = int(21e8)
-result = [inf]*n
+result = [inf]*(n+1)
 
 dajkstra(x)
-
 flag = 0
-for i in range(n):
+for i in range(1,n+1):
     if result[i] == k:
         flag = 1
-        print(i+1)
+        print(i)
 if not flag:
     print(-1)
